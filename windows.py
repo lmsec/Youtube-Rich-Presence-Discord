b42f11ae7ea7cd948f16bc4b130979f23fc44480
Lise Mira <lmira@bimandco.com>
2022-11-24 10:39:08 +0100

    several fixes

--- windows.py
+++ windows.py
@@ -1,39 +1,58 @@
 import ctypes
 from pypresence import Presence
 import time, os, sys
-from textfilters import title_filter
+from textfilters import useless_words
 
 client_id = ''  # Fake ID, put your real one here
 RPC = Presence(client_id)  # Initialize the client class
 RPC.connect()  # Start the handshake loop
 
 
+window_titles = []
+
+
 def full():
+    print("beginning")
+    song_name = None
     EnumWindows = ctypes.windll.user32.EnumWindows
     EnumWindowsProc = ctypes.WINFUNCTYPE(ctypes.c_bool, ctypes.POINTER(ctypes.c_int), ctypes.POINTER(ctypes.c_int))
     GetWindowText = ctypes.windll.user32.GetWindowTextW
     GetWindowTextLength = ctypes.windll.user32.GetWindowTextLengthW
     IsWindowVisible = ctypes.windll.user32.IsWindowVisible
 
-    titles = []
-
     def foreach_window(hwnd, lParams):
         if IsWindowVisible(hwnd):
             length = GetWindowTextLength(hwnd)
             buff = ctypes.create_unicode_buffer(length + 1)
             GetWindowText(hwnd, buff, length + 1)
-            titles.append(buff.value)
+            window_titles.append(buff.value)
+        return True
 
     EnumWindows(EnumWindowsProc(foreach_window), 0)
-    sub = 'YouTube Music'
-    for text in titles:
-        if sub in text:
-            for title in title_filter:
-                textp = text.replace(title, "")
+    title_filter = 'YouTube Music'
+    for window_title in window_titles:
+        if title_filter in window_title:
+            print(window_title)
+            song_name = window_title
+            for useless_word in useless_words:
+                song_name = song_name.replace("(" + useless_word + ")", "")
+                song_name = song_name.replace("[" + useless_word + "]", "")
+                song_name = song_name.replace(useless_word, "")
+                song_name = song_name.replace("  ", " ")
+                song_name = song_name.replace("  ", " ")
+                song_name = song_name.replace("( )", "")
 
-    print(RPC.update(state=textp + " on Youtube", details="Listening to: "))  # Set the presence
-    time.sleep(15)  # Can only update rich presence every 15 seconds
+    if song_name is not None and song_name != '':
+        song_name = (song_name[:120] + '..') if len(song_name) > 120 else song_name
+        print(RPC.update(state=song_name, details="Listening to",
+              large_image="none", large_text="none",
+              small_image="none", small_text="none"))
+        # Set the presence
+        # print(f'RPC.update(state={song_name} + " on Youtube Music", details="Listening to: ")')
+    time.sleep(5)  # Can only update rich presence every 15 seconds
+    print("ending, let's do it again")
     full()
+    print("this should not happen")
 
 
 try:
@@ -45,3 +64,4 @@ except UnboundLocalError:
     print('Please bring tab to foreground or keep it open in a separate browser!')
 finally:
     RPC.close()
+    print("the end bruv")
